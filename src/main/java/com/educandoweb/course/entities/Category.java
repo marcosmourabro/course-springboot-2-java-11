9a54d1f66bb663084d1a57dcaf1521aa83c697e6
Marcos Moura <marcosmourabro@gmail.com>
2020-11-04 19:10:29 -0300

    Category

---
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -0,0 +1,54 @@
+package com.educandoweb.course.entities;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Category(){
+
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category)) return false;
+        Category category = (Category) o;
+        return getId().equals(category.getId()) &&
+                getName().equals(category.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
+}
